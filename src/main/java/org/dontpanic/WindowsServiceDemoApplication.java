6a9873cd437268ac99f4b8fc82a36de2c1a146cf
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2017-01-01 16:36:24 +0000

    Initialized from spring.boot.io

---
+++ src/main/java/org/dontpanic/WindowsServiceDemoApplication.java
@@ -0,0 +1,12 @@
+package org.dontpanic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindowsServiceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindowsServiceDemoApplication.class, args);
+	}
+}
